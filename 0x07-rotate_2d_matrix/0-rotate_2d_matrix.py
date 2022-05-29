7ed9d70dd387431e1edb74e169cee03f839aede6
MISTOM <kigardetom2001@gmail.com>
2022-05-30 00:43:31 +0300

    Pycodestyle pep 8 styleguide

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,28 +2,27 @@
 """Rotate 2D matrix module
 """
 from typing import List
+
+
 def rotate_2d_matrix(matrix: List[List[int]]):
     """Rotates a 2D matrix 90 Degrees Clockwise"""
-    
     l = 0
     r = len(matrix) - 1
     while (l < r):
         for i in range(r - l):
             top, bottom = l, r
-
-            #save the topLeft value temporarily
+            # save the topLeft value temporarily
             temp = matrix[top][l + i]
-            
-            #move bottomLeft into topLeft
+            # move bottomLeft into topLeft
             matrix[top][l + i] = matrix[bottom - i][l]
 
-            #move bottomRight into bottomLeft
+            # move bottomRight into bottomLeft
             matrix[bottom - i][l] = matrix[bottom][r - i]
 
-            #move topRight into bottomRight
+            # move topRight into bottomRight
             matrix[bottom][r - i] = matrix[top + i][r]
 
-            #move top
+            # move topLeft to topRight
             matrix[top + i][r] = temp
-        r-=1
-        l+=1
+        r -= 1
+        l += 1
