c9c2b695c8a9eb3158ca500a19d28c9bb2f01971
MISTOM <kigardetom2001@gmail.com>
2022-06-20 12:26:36 +0300

    add get input function

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""N queens solution finder module.
+"""
+import sys
+
+
+solutions = []
+"""The list of possible solutions to the N queens problem.
+"""
+n = 0
+"""The size of the chessboard.
+"""
+pos = None
+"""The list of possible positions on the chessboard.
+"""
+
+
+def get_input():
+    """Retrieves and validates this program's argument.
+    Returns:
+        int: The size of the chessboard.
+    """
+    global n
+    n = 0
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return n
